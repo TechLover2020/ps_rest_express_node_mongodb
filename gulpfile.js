a5eb6a02fb0dfa0f38e8d809ba7c3fe580b790a0
John Jose Morris <morrij87@MC-S091751.local>
2017-12-08 13:36:23 +0000

    added nodemon for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@ var gulp = require('gulp')
 var nodemon = require('gulp-nodemon')
 var port = process.env.PORT || 3000
 
-gulp.task('start', function () {
+gulp.task('default', function () {
   nodemon({
     script: 'index.js'
     , ext: 'js html',
-    
+    env: {
+      PORT: 8000
+    },
+    ignore: ['./node_modules/**']
   })
 })
