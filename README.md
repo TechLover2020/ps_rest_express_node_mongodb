35d3cbc48890df75178cf294e6a5d5b567b8e9eb
John Jose Morris <morrij87@MC-S091751.local>
2017-12-12 15:57:06 +0000

    added a readme

---
+++ README.md
@@ -0,0 +1,20 @@
+Project
+
+    API project of a library. Uses Express, mongoose .. 
+        get, post, delete, put 
+
+Setup
+
+    1. Install mongoose db
+    2. Create a Local db
+    3. Start the mongod server
+    4. Gulp on terminal
+    5. Use postman to hit endpoints
+    
+Endpoints
+    
+    http://localhost:8081/api/Books
+    http://localhost:8081/api/Books/5a2aa18175faa5168d6c0347
+    
+    
+    
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var mongoose = require('mongoose')
 var db = mongoose.connect('mongodb://localhost/bookAPI') // open a connecction to db
 var Book = require('./models/bookModel') // mongoose gets data out of mongodb via model
+
 var app = express()
 var bodyParser = require('body-parser') // read body and parse into json object
 
@@ -11,10 +12,9 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 bookRouter =require('./Routes/bookRoutes')(Book)
 
-
 app.use('/api', bookRouter)
 
-// http://localhost:3001/
+// http://localhost:8081/
 // on browser
 app.get('/', (req, res) => {
   res.send('Welcome to API')
